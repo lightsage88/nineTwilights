dcf7c05fb3ea8ccec0f5da13b3fe19a1584af0b8
Adrian Rosales <adrian.e.rosales@gmail.com>
2018-05-06 23:31:51 -0700

    first commit

---
+++ src/reducers/9T.js
@@ -0,0 +1,17 @@
+
+
+const initialState = {
+	siteInfo: {}
+}
+
+const siteInfo = (state=initialState, action) => {
+	switch(action.type) {
+
+
+
+		default:
+			return {}
+	}
+}
+
+export default siteInfo
---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+import siteInfo from './9T';
+
+const rootReducer = siteInfo;
+
+export default rootReducer;
