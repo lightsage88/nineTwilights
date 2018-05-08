fe3f3ae2d5fdff0ba3ad31f43bd377af2b07b10a
Adrian Rosales <adrian.e.rosales@gmail.com>
2018-05-07 18:15:13 -0700

    about mobile looking good!

--- src/components/about.js
+++ src/components/about.js
@@ -1,8 +1,27 @@
 import React from 'react';
-
+import norsePatternGraphic from '../staticAssets/NORSE-PATTERN-GRAPHIC.png';
+import './about.css';
 export default function About(props){
 	return (
-		<h3>About under construction!</h3>
+		<div className='aboutMain'>
+			<img className='norsePatternGraphic' src={norsePatternGraphic}/>
+			<section className='aboutText'>
+				<h3>RAGNAROK: The Twilight of the Gods</h3>
+				<p>
+					Eons ago, nine of the gods, lead by Odin, made a pact: as they died in their final battle, they would battle
+					they would be reincarnated upon Midgard and take the chance to live new lives free of their past transgressions.
+					<br/><br/>
+					But as the gods survived, so too did their enemies.
+					<br/><br/>
+					Wanda is a sixteen-year-old girl who dreams of independence and respect from her family and hometown. But her
+					world is rocked when she meets a strange man who introduces himself as Baldur, New King of Asgard, and her as his
+					father Odin reborn.
+					The gods' breach of fate allowed an ancient enemy, whose identity reamins a mystery to Baldur, to seek vengeance.
+					Now, Wanda and Baldur must find the remaining eight gods and bring peace to the nine worlds - before it's too late.
+				</p>
+			</section>
+			<img id='closingNorse' className='norsePatternGraphic' src={norsePatternGraphic}/>
+		</div>
 
 		);
 }
