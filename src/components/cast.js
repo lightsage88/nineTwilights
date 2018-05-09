19b1f1d4bfb2f8aed3836d46defc06de249ad739
Adrian Rosales <adrian.e.rosales@gmail.com>
2018-05-09 11:25:30 -0700

    got the castpage looking gangsta, thanks reactstrap layout!

--- src/components/cast.js
+++ src/components/cast.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import norseDivider from '../staticAssets/9T-text-divider.png';
 import wandaPic from '../staticAssets/Wanda--Character-pic-.png';
 import baldurPic from '../staticAssets/Baldur-pic.png';
 import sariPic from '../staticAssets/Sari-pic.png';
@@ -10,38 +12,50 @@ export default function Cast(props){
 
 		
 		<div className='castMain'>
+		<Container>
 				<section>
 				<br/><br/><br/>
 					<h3 className='castHeader'>CAST</h3>
-					
+					<img className='norseDivider norseDivider1' src={norseDivider}/>
 						<section className='castEntries'>
+						<Row>
+
 							<div className='wandaDusekova'>
-								<img className='castMemberPicture wandaPic' src={wandaPic} alt='Wanda Dusekova'/>
-								<h3 className='castMemberName wandaName'>WANDA DUSEKOVA</h3>
+								<Col xs='6' className='wanda1'><img className='castMemberPicture wandaPic' src={wandaPic} alt='Wanda Dusekova'/></Col>
+								<Col xs='6' className='wanda2'><h3 className='castMemberName wandaName'>WANDA DUSEKOVA</h3>
 								<p className='castMemberSummary wandaSummary'>An ordinary young woman from the Czech Republic - who happens to be the reincarnation of Odin.
 								<br/><br/>
 								Kindhearted and fiercely loyal under a hardened shell, Wanda reluctantly takes on the responsibility of saving the world.</p>
+								</Col>
 							</div>
+						</Row>
+						<img className='norseDivider norseDivider2' src={norseDivider}/>
+						<Row>
 							<div className='baldurOdinsson'>
-								<img className='castMemberPicture baldurPic' src={baldurPic} alt='Baldur Odinsson'/>
-								<h3 className='castMemberName baldurName'>BALDUR ODINSSON</h3>
+								<Col xs='6' className='baldur1'><img className='castMemberPicture baldurPic' src={baldurPic} alt='Baldur Odinsson'/></Col>
+								<Col xs='6' className='baldur2'><h3 className='castMemberName baldurName'>BALDUR ODINSSON</h3>
 								<p className='castMemberSummary baldurSummary'>King of New Asgard and leader of the Aesir. Baldur
 								has come to Wanda, seeking her help to save the nine worlds from destruction.
 								<br/><br/>Baldur's heart is always in the right place, but his dedication to his
 								duties as king causes conflict.</p>
-
+								</Col>
 							</div>
+						</Row>
+						<img className='norseDivider norseDivider3' src={norseDivider}/>
+						<Row>
 							<div className='sari'>
-								<img className='castMemberPicture sariPic' src={sariPic}/>
-								<h3 className='castMemberName sariName'>SARI</h3>
+								<Col xs='6' className='sari1'><img className='castMemberPicture sariPic' src={sariPic}/></Col>
+								<Col xs='6' className='sari2'><h3 className='castMemberName sariName'>SARI</h3>
 								<p className='castMemberSummary sariSummary'>Sari quickly befriended Wanda after literally 
 								running into her on the streets of Jakarta. She can be hard to get along with, but there is no one more loyal.
 								<br/><br/>Sari is resourceful, clever, and more than a little harsh, but she's got
 								Wanda's back 'til the very end.</p>
-
+								</Col>
 							</div>
+						</Row>
 						</section>
 				</section>
+		</Container>		
 		</div>
 	
 
