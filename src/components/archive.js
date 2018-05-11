e530785adc08c72819e37d4566159b47f06c4711
Adrian Rosales <adrian.e.rosales@gmail.com>
2018-05-11 00:12:29 -0700

    got the category picker working just right, now i need to dynamically create options in the second select menu to reflect what's in the state

--- src/components/archive.js
+++ src/components/archive.js
@@ -7,6 +7,10 @@ import './archive.css';
 export class Archive extends React.Component{
 	constructor(props){
 		super(props);
+		this.state = {
+			category: '',
+			pages: []
+		}
 	}	
 
 	componentDidMount(){
@@ -23,12 +27,50 @@ export class Archive extends React.Component{
 		this.props.dispatch(displayPage(page));
 	}
 
+	categorySelect(e){
+		console.log('categorySelect running');
+		e.preventDefault();
+		console.log(e.target.value);
+		let array = [];
+		let categoryType = e.target.value;
+		console.log(categoryType);
+		let library = this.props.comicPages;
+		for(let i=0; i<=library.length-1; i++){
+			let string = library[i].fileName;
+			if(string.includes(categoryType)) {
+				array.push(string);
+			} else {
+				console.log('nope');
+			}
+		}
+
+		array.sort(function(a,b){
+			return a + b;
+		});
+
+		console.log(array);
+
+		this.setState({
+			pages: array
+		});
+	}
+
 	render(){
 		return (
 				<div className='archiveMain'>
 				<button onClick={(e)=>this.testButton(e)}className='showAnyPage'>show a page</button>
 					<img className='norsePatternGraphic' src={norsePatternGraphic}/>
-											<h3 className='pageNumber'>placeholder</h3>
+										<select onChange={(e)=>this.categorySelect(e)}>
+											<option selected>SELECT CATEGORY</option>
+											<option value='Chp. 1'>CHAPTER 1</option>
+											<option value='Chp. 2'>CHAPTER 2</option>
+											<option value='GA&W'>GUEST ART & WRITING</option>
+											<option value='ACA'>Anne Clip Art</option>
+										</select>
+										{this.state.pages !== [] ?
+										<select>SUP</select>
+										: null
+										}
 
 					<section className='comicBookZone'>
 						<div className='pagePicker'>
