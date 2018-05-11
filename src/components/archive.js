b41a36bfa341bce4538a4c70748825537dcb6760
Adrian Rosales <adrian.e.rosales@gmail.com>
2018-05-11 13:49:08 -0700

    using the prev button now works! just gotta apply the same stuff to the next button

--- src/components/archive.js
+++ src/components/archive.js
@@ -9,7 +9,8 @@ export class Archive extends React.Component{
 		super(props);
 		this.state = {
 			category: '',
-			pages: []
+			pages: [],
+			pageNumber: ''
 		}
 	}	
 
@@ -28,31 +29,68 @@ export class Archive extends React.Component{
 	}
 
 	categorySelect(e){
-		console.log('categorySelect running');
 		e.preventDefault();
-		console.log(e.target.value);
 		let array = [];
 		let categoryType = e.target.value;
-		console.log(categoryType);
 		let library = this.props.comicPages;
 		for(let i=0; i<=library.length-1; i++){
 			let string = library[i].fileName;
 			if(string.includes(categoryType)) {
 				array.push(string);
 			} else {
-				console.log('nope');
 			}
 		}
-
 		array.sort(function(a,b){
 			return a + b;
 		});
-
-		console.log(array);
-
 		this.setState({
 			pages: array
 		});
+	}
+
+	pageSelect(e){
+		e.preventDefault();
+		console.log('pageSelect running');
+		console.log(e.target.value);
+		let pageNumber = e.target.value;
+		this.setState({
+			pageNumber: pageNumber
+		});
+		this.props.dispatch(displayPage(pageNumber));
+	}
+
+	prevPage(e){
+		console.log('prevPage running');
+		console.log(e);
+		
+		let library = this.state.pages;
+		let currentPage = this.state.pageNumber;
+		let prevPage = '';
+		console.log(library);
+		console.log(currentPage);
+		for(let i = 0; i<=library.length-1; i++){
+			if(library[i] == currentPage){
+				console.log(i);
+				console.log('gotta match');
+				prevPage = library[i-1];
+			}
+		}
+		console.log(prevPage);
+		this.setState({
+			pageNumber: prevPage
+		});
+		this.props.dispatch(displayPage(prevPage));
+		
+
+	}
+
+	nextPage(e){
+		console.log('nextPage running');
+		console.log(e);
+		let select = document.getElementById('pageSelector');
+		let options = select.options;
+		
+
 	}
 
 	render(){
@@ -60,21 +98,21 @@ export class Archive extends React.Component{
 				<div className='archiveMain'>
 				<button onClick={(e)=>this.testButton(e)}className='showAnyPage'>show a page</button>
 					<img className='norsePatternGraphic' src={norsePatternGraphic}/>
-										<select onChange={(e)=>this.categorySelect(e)}>
-											<option selected>SELECT CATEGORY</option>
+										<select defaultValue='SELECT CATEGORY' onChange={(e)=>this.categorySelect(e)}>
+											<option >SELECT CATEGORY</option>
 											<option value='Chp. 1'>CHAPTER 1</option>
 											<option value='Chp. 2'>CHAPTER 2</option>
 											<option value='GA&W'>GUEST ART & WRITING</option>
 											<option value='ACA'>Anne Clip Art</option>
 										</select>
-										<select>
-											
+										<select id='pageSelector' onChange={(e)=>this.pageSelect(e)}>
+											<option>SELECT PAGE</option>
 											{
 											 //if this.state.pages is not just [] then fill it up with its contents, other wise NULL
 											 this.state.pages !== [] ? (this.state.pages).map(function(page, index){
-											 	return <option key={index} value={page.fileName}>{index+1}</option>
+											 	return <option key={index} value={page}>{index+1}</option>
 											 })
-											 : <option value='' placeholder='N/A' selected disabled>N/A</option>
+											 : <option placeholder='N/A' disabled>N/A</option>
 
 											}
 										</select>
@@ -82,8 +120,8 @@ export class Archive extends React.Component{
 
 					<section className='comicBookZone'>
 						<div className='pagePicker'>
-							<p className='pageFlip previousPage'>PREVIOUS</p>
-							<p className='pageFlip nextPage'>NEXT</p>
+							<p id='prev' onClick={(e)=>this.prevPage(e)} className='pageFlip previousPage'>PREVIOUS</p>
+							<p id='next' onClick={(e)=>this.nextPage(e)} className='pageFlip nextPage'>NEXT</p>
 						</div>
 							{this.props.nowPlaying !== '' ?
 							<img className='nowPlaying' src={this.props.nowPlaying}/>
