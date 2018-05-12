9912aac94353cc2ec7642ffd43c838b8f4148471
Adrian Rosales <adrian.e.rosales@gmail.com>
2018-05-11 17:26:52 -0700

    going live

--- src/config.js
+++ src/config.js
@@ -3,5 +3,8 @@ module.exports = {
 	DATABASE_URL: process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://lightsage88:Walruses8@ds161029.mlab.com:61029/9t',
 	JWT_SECRET: process.env.JWT_SECRET || 'pepperTheCat',
 	JWT_EXPIRY: process.env.JWT_EXPIRY || '15m',
-	API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "https://localhost:5000"
+	API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "https://enigmatic-ravine-86207.herokuapp.com"
 };
+
+
+//"https://localhost:5000"
